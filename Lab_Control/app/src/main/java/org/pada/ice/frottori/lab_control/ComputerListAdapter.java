aee1411df15de2b608b624ef63d45ceef21b3e34
frottori <frossovarsou@gmail.com>
2025-06-16 15:53:23 +0300

    Refactor ComputerListAdapter to improve variable naming and unused variables

--- Lab_Control/app/src/main/java/org/pada/ice/frottori/lab_control/ComputerListAdapter.java
+++ Lab_Control/app/src/main/java/org/pada/ice/frottori/lab_control/ComputerListAdapter.java
@@ -10,30 +10,27 @@ import androidx.annotation.NonNull;
 
 public class ComputerListAdapter extends ArrayAdapter<String> {
     private final Context context;
-    private final String[] computers;
-    private final Boolean[] onlineStatus;
+    private final Boolean[] online_computers;
 
-    public ComputerListAdapter(Context context, String[] computers, Boolean[] onlineStatus) {
+    public ComputerListAdapter(Context context, String[] computers, Boolean[] online_computers) {
         super(context, android.R.layout.simple_list_item_multiple_choice, computers);
         this.context = context;
-        this.computers = computers;
-        this.onlineStatus = onlineStatus;
+        this.online_computers = online_computers;
     }
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int pos, View convertView, ViewGroup parent) {
         // Use the default layout for multiple choice items
-        View view = super.getView(position, convertView, parent);
+        View view = super.getView(pos, convertView, parent);
         TextView textView = (TextView) view.findViewById(android.R.id.text1);
 
         // Change text color based on online status
-        if (onlineStatus[position]) {
+        if (online_computers[pos]) {
             textView.setTextColor(context.getResources().getColor(android.R.color.holo_green_dark));  // Online = green
         } else {
             textView.setTextColor(context.getResources().getColor(android.R.color.holo_red_dark));    // Offline = red
         }
-
         return view;
     }
 }
